b8d214f46a43d5aa46443ecaac6ea80f0ec1edd9
User <asqarservis0000@gmail.com>
2025-08-23 14:30:08 +0500

    updated

--- apps/views.py
+++ apps/views.py
@@ -1,37 +1,12 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework.generics import CreateAPIView, ListAPIView, DestroyAPIView, UpdateAPIView, RetrieveAPIView
+from rest_framework.generics import DestroyAPIView, UpdateAPIView, RetrieveAPIView, CreateAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from apps.models import Blog, BlogImages
 from apps.serializers import BlogModelSerializer, BlogImagesModelSerializer
 
 
-@extend_schema(tags=['blog'])
-class BlogCreateAPIView(CreateAPIView):
-    serializer_class = BlogModelSerializer
-    queryset = Blog.objects.all()
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-from drf_spectacular.utils import extend_schema, OpenApiParameter
-from rest_framework.generics import CreateAPIView, ListAPIView
-
-from apps.serializers import FollowingModelSerializer
-from authentication.models import Follow
-
-
-@extend_schema(tags=['follow'])
-class FollowingCreateAPIView(CreateAPIView):
-    serializer_class = FollowingModelSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(follower=self.request.user)
-
-
-@extend_schema(tags=['blog'])
-class BlogListAPIView(ListAPIView):
-    serializer_class = BlogModelSerializer
-    queryset = Blog.objects.all()
-    permission_classes = [IsAuthenticated]
+# Create your views here.
 
 
 @extend_schema(tags=['blog'])
@@ -95,29 +70,3 @@ class BlogImagesListAPIView(ListAPIView):
     queryset = BlogImages.objects.all()
     permission_classes = [IsAuthenticated]
 
-@extend_schema(tags=['follow'], parameters=[
-    OpenApiParameter(
-        name='follower',
-        description='enter follower',
-        required=False,
-        type=str,
-    ),
-    OpenApiParameter(
-        name='following',
-        description='enter follower',
-        required=False,
-        type=str,
-    )])
-class FollowsListAPiView(ListAPIView):
-    queryset = Follow.objects.all()
-    serializer_class = FollowingModelSerializer
-
-    def get_queryset(self):
-        query = super().get_queryset()
-        follower = self.request.query_params.get('follower')
-        following = self.request.query_params.get('following')
-        if follower:
-            query = query.filter(follower=self.request.user)
-        if following:
-            query = query.filter(following=self.request.user)
-        return query
