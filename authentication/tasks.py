03d5225267bf53d4920ba24ae77c576192e2443b
User <asqarservis0000@gmail.com>
2025-08-27 17:05:48 +0500

    updated

--- authentication/tasks.py
+++ authentication/tasks.py
@@ -10,18 +10,11 @@ def send_code_email(user_email: dict, code):
     subject = "Email Verification"
     from_email = EMAIL_HOST_USER
     to = [user_email.get('email')]
-
-    # Context (dynamic data)
     context = {
         "code": code,
-        "verify_url": f"http://localhost:8000/verify/{code}"
     }
-    print(context)
-    # Render HTML
     html_content = render_to_string("verification_email.html", context=context)
-    text_content = f"Your verification code is {code}"  # fallback plain text
-
-    # Create email
+    text_content = f"Your verification code is {code}"
     msg = EmailMultiAlternatives(subject, text_content, from_email, to)
     msg.attach_alternative(html_content, "text/html")
     msg.send()
