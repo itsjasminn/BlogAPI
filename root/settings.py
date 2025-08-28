7edf78b8d2ca7ea9fb2e0aa64aca94abd9426bc7
User <asqarservis0000@gmail.com>
2025-08-28 14:43:48 +0500

    updated

--- root/settings.py
+++ root/settings.py
@@ -33,13 +33,15 @@ INSTALLED_APPS = [
     'drf_spectacular_sidecar',
     # JWT
     'rest_framework_simplejwt',
-
+    # graphana
+    'django_prometheus',
     # filter
     'django_filters'
 
 ]
 
 MIDDLEWARE = [
+    'django_prometheus.middleware.PrometheusBeforeMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_prometheus.middleware.PrometheusAfterMiddleware'
 ]
 
 ROOT_URLCONF = 'root.urls'
--- root/urls.py
+++ root/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from prometheus_client import start_http_server, Summary, Counter, Gauge, make_wsgi_app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +12,22 @@ urlpatterns = [
     path('api/v1/', include('authentication.urls')),
     path('api/v1/', include('apps.urls')),
 
+]
+
+from django.utils.deprecation import MiddlewareMixin
+from django.http import HttpResponse
+
+
+class MetricsMiddleware(MiddlewareMixin):
+    def __call__(self, request):
+        return self.get_response(request)
+
+
+def metrics_view(request):
+    from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
+    return HttpResponse(generate_latest(), content_type=CONTENT_TYPE_LATEST)
+
+
+urlpatterns += [
+    path("metrics/", metrics_view),
 ]
