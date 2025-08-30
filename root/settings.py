cf70f79ac80fb3e0debe6e8a9a0b39d5b302fe01
User <asqarservis0000@gmail.com>
2025-08-30 18:42:36 +0500

    updated

--- root/settings.py
+++ root/settings.py
@@ -33,15 +33,12 @@ INSTALLED_APPS = [
     'drf_spectacular_sidecar',
     # JWT
     'rest_framework_simplejwt',
-    # graphana
-    'django_prometheus',
     # filter
     'django_filters'
 
 ]
 
 MIDDLEWARE = [
-    'django_prometheus.middleware.PrometheusBeforeMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,7 +46,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_prometheus.middleware.PrometheusAfterMiddleware'
 ]
 
 ROOT_URLCONF = 'root.urls'
--- root/urls.py
+++ root/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from prometheus_client import start_http_server, Summary, Counter, Gauge, make_wsgi_app
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
@@ -10,24 +8,6 @@ urlpatterns = [
     # Optional UI:
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/v1/', include('authentication.urls')),
-    path('api/v1/', include('apps.urls')),
+    path('api/v1/', include('apps.urls'))
 
-]
-
-from django.utils.deprecation import MiddlewareMixin
-from django.http import HttpResponse
-
-
-class MetricsMiddleware(MiddlewareMixin):
-    def __call__(self, request):
-        return self.get_response(request)
-
-
-def metrics_view(request):
-    from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
-    return HttpResponse(generate_latest(), content_type=CONTENT_TYPE_LATEST)
-
-
-urlpatterns += [
-    path("metrics/", metrics_view),
 ]
